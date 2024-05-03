4c876a3e6e8118db9a17cf72449cf8ef08e4899b
Tareq <tareqnmd@gmail.com>
2024-05-03 12:58:55 +0600

    create portal added

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,28 +1,33 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { PropTypes } from 'prop-types';
 import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import './modal.css';
 const Modal = ({ open, title, children, onCancel, onOk }) => {
 	const [mounted, setMounted] = useState(false);
 	useEffect(() => {
 		setMounted(true);
 	}, []);
-	return mounted && open ? (
-		<div className="modal-container">
-			<div
-				onClick={onCancel}
-				className="modal-bg"
-			></div>
-			<div className="modal-content">
-				<div className="modal-header">
-					{title} <button onClick={onCancel}>&times;</button>
-				</div>
-				<div className="modal-body">{children}</div>
-				<div className="modal-footer">
-					{onOk && <button onClick={onOk}>Ok</button>}
-				</div>
-			</div>
-		</div>
-	) : null;
+	return mounted && open
+		? createPortal(
+				<div className="modal-container">
+					<div
+						onClick={onCancel}
+						className="modal-bg"
+					></div>
+					<div className="modal-content">
+						<div className="modal-header">
+							{title} <button onClick={onCancel}>&times;</button>
+						</div>
+						<div className="modal-body">{children}</div>
+						<div className="modal-footer">
+							{onOk && <button onClick={onOk}>Ok</button>}
+						</div>
+					</div>
+				</div>,
+				document.body
+		  )
+		: null;
 };
 
 Modal.propTypes = {
